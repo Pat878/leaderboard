016b88045a7b38854fb70c5090ff3c159849cb57
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-02-04 10:03:12 -0500

    Styling

--- src/App.js
+++ src/App.js
@@ -1,118 +1,122 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Jumbotron, Button, Table } from 'react-bootstrap';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Jumbotron, Button, Table } from "react-bootstrap";
 
-var axios = require('axios');
+var axios = require("axios");
 
 class LeaderBoard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: [],
+      active: false
+    };
 
-   constructor(props) {
-        super(props)
-      this.state = {
-        name: [],
-        active: false
+    this.allTimeClick = this.allTimeClick.bind(this);
+    this.recentClick = this.recentClick.bind(this);
+  }
 
-         }
+  componentDidMount() {
+    this.recentClick();
+  }
 
-  this.allTimeClick = this.allTimeClick.bind(this);
-  this.recentClick = this.recentClick.bind(this);
-      }
+  allTimeClick() {
+    axios
+      .get("https://fcctop100.herokuapp.com/api/fccusers/top/alltime")
+      .then(res => {
+        this.setState({ name: res.data, active: true });
+      });
+  }
 
+  recentClick() {
+    axios
+      .get("https://fcctop100.herokuapp.com/api/fccusers/top/recent")
+      .then(res => {
+        this.setState({ name: res.data, active: false });
+      });
+  }
 
-  componentDidMount(){
-
-      this.recentClick()
-
-    }
-
-  allTimeClick(){
-axios.get("https://fcctop100.herokuapp.com/api/fccusers/top/alltime").then(res => {
-
-  this.setState({name: res.data, active: true});
-    });  }
-
-recentClick(){
-  axios.get("https://fcctop100.herokuapp.com/api/fccusers/top/recent").then(res => {
-
-this.setState({name: res.data, active: false});
-
+  render() {
+    const namesToRender = this.state.name.map(val => {
+      return <tr key={val.username}> {val.username}</tr>;
     });
-}
 
-render () {
-   const namesToRender = this.state.name.map(val => {
-     return (
-    <tr key={val.username}>  {val.username}</tr>
+    const imgsToRender = this.state.name.map(val => {
+      return (
+        <tr key={val.username}>
+          <img src={val.img} height="18" alt={val.img} />
+        </tr>
+      );
+    });
 
-       )
+    const allTimeRender = this.state.name.map(val => {
+      return <tr key={val.username}> {val.alltime}</tr>;
+    });
 
-   })
-
-   const imgsToRender = this.state.name.map(val => {
-     return (
-    <tr key={val.username}><img src={val.img} height="18" alt={val.img}/></tr>
-
-       )
-
-   })
-
-   const allTimeRender = this.state.name.map(val => {
-     return (
-       <tr key={val.username}> {val.alltime}</tr>
-
-       )
-
-   })
-
-      const recentRender = this.state.name.map(val => {
-     return (
-       <tr key={val.username}> {val.recent}</tr>
-
-       )
-
-   })
+    const recentRender = this.state.name.map(val => {
+      return <tr key={val.username}> {val.recent}</tr>;
+    });
 
     return (
       <div className="container">
         <center>
-          <Jumbotron bsClass="jumbotron"><h1>FreeCodeCamp LeaderBoard</h1></Jumbotron>
+          <Jumbotron bsClass="jumbotron">
+            <h1>FreeCodeCamp LeaderBoard</h1>
+          </Jumbotron>
 
-        <Button bsClass={this.state.active ? 'btn active': 'btn'} onClick={this.allTimeClick} >
-          Alltime</Button>
-        <Button bsClass={this.state.active ? 'btn' : 'btn active'} onClick={this.recentClick} >Recent </Button>
-
-            </center>
+          <Button
+            bsClass={this.state.active ? "btn active" : "btn"}
+            onClick={this.allTimeClick}
+          >
+            Alltime
+          </Button>
+          <Button
+            bsClass={this.state.active ? "btn" : "btn active"}
+            onClick={this.recentClick}
+          >
+            Recent{" "}
+          </Button>
+        </center>
         <Table className="table table-bordered">
-<thead>
-  <tr><th>Username</th>
-<th>Avatar</th>
-  <th>Alltime Points <div className={this.state.active ? 'fa fa-arrow-up': 'fa fa-arrow-down'}></div></th>
- <th>Recent Points <div className={this.state.active ? 'fa fa-arrow-down': 'fa fa-arrow-up'}></div></th>
-</tr>
-</thead>
-    <tbody>
-        <td>{namesToRender}</td>
-       <td>{imgsToRender}</td>
-         <td>{allTimeRender}</td>
-         <td>{recentRender}</td>
-
-</tbody>
-
-       </Table>
-
-       </div>
-      )
-     }
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Avatar</th>
+              <th>
+                Alltime Points{" "}
+                <div
+                  className={
+                    this.state.active ? "fa fa-arrow-up" : "fa fa-arrow-down"
+                  }
+                />
+              </th>
+              <th>
+                Recent Points{" "}
+                <div
+                  className={
+                    this.state.active ? "fa fa-arrow-down" : "fa fa-arrow-up"
+                  }
+                />
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <td>{namesToRender}</td>
+            <td>{imgsToRender}</td>
+            <td>{allTimeRender}</td>
+            <td>{recentRender}</td>
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
 
 /*ReactDOM.render(
   <LeaderBoard />, document.getElementById("app"));*/
 
-
 export default LeaderBoard;
-
-
 
 /* class App extends Component {
   render() {
